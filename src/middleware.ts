fc9282ee2f4f3c0f58a2dfdc69a98f904ed207ef
TylerIllman <tyler.illman.productions@gmail.com>
2024-07-03 21:16:07 +0800

    commit prior to overwriting trpc.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,21 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/forum(.*)"]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/sign-in",
+    "/sign-up",
+    "/sign-in/(.*)",
+    "/sign-up/(.*)",
+    "/api/(.*)",
+    "/api/message",
+    "/pricing",
+  ],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
