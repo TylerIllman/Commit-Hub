8d25766ac28af04d3925c484a2dd80d0e3a8786c
TylerIllman <tyler.illman.productions@gmail.com>
2024-07-03 18:39:04 +0800

    Convert to using turso db

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,17 +1,33 @@
 import { PrismaClient } from "@prisma/client";
 
-import { env } from "~/env";
+// import { env } from "~/env";
+import dotenv from "dotenv";
 
-const createPrismaClient = () =>
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+import { createClient } from "@libsql/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
-};
+dotenv.config();
 
-export const db = globalForPrisma.prisma ?? createPrismaClient();
+// TODO: Convert this to use the env.js file so it is dynamic (and not the 'dotenv')
+const libsql = createClient({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+});
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+const adapter = new PrismaLibSQL(libsql);
+
+export const db = new PrismaClient({ adapter });
+
+// const createPrismaClient = () =>
+//   new PrismaClient({
+//     log:
+//       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+//   });
+//
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: ReturnType<typeof createPrismaClient> | undefined;
+// };
+//
+// export const db = globalForPrisma.prisma ?? createPrismaClient();
+//
+// if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
