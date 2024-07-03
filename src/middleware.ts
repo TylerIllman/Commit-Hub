a9120df035884f3d3a49cb9e1b01d3be0522fe2e
TylerIllman <tyler.illman.productions@gmail.com>
2024-07-03 21:09:05 +0800

    added Middleware auth PROPER

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,29 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isTenantRoute = createRouteMatcher([
+  "/organization-selector(.*)",
+  "/orgid/(.*)",
+]);
+
+const isTenantAdminRoute = createRouteMatcher([
+  "/orgId/(.*)/memberships",
+  "/orgId/(.*)/domain",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  // Restrict admin routes to users with specific permissions
+  if (isTenantAdminRoute(req)) {
+    auth().protect((has) => {
+      return (
+        has({ permission: "org:sys_memberships:manage" }) ||
+        has({ permission: "org:sys_domains_manage" })
+      );
+    });
+  }
+  // Restrict organization routes to signed in users
+  if (isTenantRoute(req)) auth().protect();
+});
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
