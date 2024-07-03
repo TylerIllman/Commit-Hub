78390bc2afdf041c6ffa70b545d41cdc78f649ab
TylerIllman <tyler.illman.productions@gmail.com>
2024-07-03 21:11:32 +0800

    edit Clerk middleware to auth based on isSignedin

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isTenantRoute = createRouteMatcher([
-  "/organization-selector(.*)",
-  "/orgid/(.*)",
-]);
-
-const isTenantAdminRoute = createRouteMatcher([
-  "/orgId/(.*)/memberships",
-  "/orgId/(.*)/domain",
-]);
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/forum(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  // Restrict admin routes to users with specific permissions
-  if (isTenantAdminRoute(req)) {
-    auth().protect((has) => {
-      return (
-        has({ permission: "org:sys_memberships:manage" }) ||
-        has({ permission: "org:sys_domains_manage" })
-      );
-    });
-  }
-  // Restrict organization routes to signed in users
-  if (isTenantRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
