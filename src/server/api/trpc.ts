5c9d0e75754ac07cc42946249b415468304a0b63
TylerIllman <tyler.illman.productions@gmail.com>
2024-07-03 21:37:27 +0800

    added clerk provider back (it disappeard for some reason) add middle ware back, added user to ctx

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -6,6 +6,7 @@
  * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
  * need to use are documented accordingly near the end.
  */
+import { currentUser } from "@clerk/nextjs/server";
 import { initTRPC } from "@trpc/server";
 import superjson from "superjson";
 import { ZodError } from "zod";
@@ -25,8 +26,10 @@ import { db } from "~/server/db";
  * @see https://trpc.io/docs/server/context
  */
 export const createTRPCContext = async (opts: { headers: Headers }) => {
+  const user = currentUser();
   return {
     db,
+    user,
     ...opts,
   };
 };
