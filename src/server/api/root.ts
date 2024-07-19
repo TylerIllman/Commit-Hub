f70617ed924a6ff89012e0023b0a9ac41e8feceb
TylerIllman <tyler.illman.productions@gmail.com>
2024-07-19 15:23:16 +0800

    Delete post router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { authRouter } from "./routers/auth";
 import { userRouter } from "./routers/user";
@@ -10,7 +9,6 @@ import { streaksRouter } from "./routers/streaks";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   auth: authRouter,
   user: userRouter,
   streaks: streaksRouter,
