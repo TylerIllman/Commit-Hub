d559b99e5979ea72e4a99b4540752e9a332b6415
TylerIllman <tyler.illman.productions@gmail.com>
2024-07-19 19:31:14 +0800

    Responsive home page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,10 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-center text-8xl font-bold">Turn Plans Into</h1>
-      <div className="p-2"></div>
-      <h1 className="text-center text-8xl font-bold">
-        <span className="rounded-md bg-primary px-4 py-1">Commitments</span>
-      </h1>
+    <main className="flex min-h-screen flex-col items-center justify-center text-center text-5xl font-bold md:text-6xl lg:text-8xl">
+      <h1>Turn Plans Into</h1>
+      <div className="sm:p-0.5 lg:p-2"></div>
+      <h1 className="rounded-md bg-primary px-4 py-1">Commitments</h1>
       <div className="p-4"></div>
     </main>
   );
